4307b5dedf3417bafea8c1456d9f3d1ba7e76a5b
madj <madj@ailleron.com>
2024-12-11 14:43:21 +0100

    About preview

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-  />
+  <>
+    <AboutPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      content={widgetFor('body')}
+    />
+    <p>About Preview oo</p>
+  </>
 )
 
 AboutPagePreview.propTypes = {
