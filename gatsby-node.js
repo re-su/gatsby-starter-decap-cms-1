7613ca1bc7604ac3bc56ddc9e44e5cdf4df52f3a
madj <madj@ailleron.com>
2024-12-11 14:59:26 +0100

    Add custom gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,10 +2,10 @@ const _ = require('lodash')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
-exports.createPages = ({ actions, graphql }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
 
-  return graphql(`
+  const result = await graphql(`
     {
       allMarkdownRemark(limit: 1000) {
         edges {
@@ -22,51 +22,49 @@ exports.createPages = ({ actions, graphql }) => {
         }
       }
     }
-  `).then((result) => {
-    if (result.errors) {
-      result.errors.forEach((e) => console.error(e.toString()))
-      return Promise.reject(result.errors)
+  `)
+
+  if (result.errors) {
+    result.errors.forEach((e) => console.error(e.toString()))
+    throw result.errors
+  }
+
+  const posts = result.data.allMarkdownRemark.edges
+
+  // Create pages for each Markdown file
+  posts.forEach((edge) => {
+    const id = edge.node.id
+    const slug = edge.node.fields.slug
+    const templateKey = edge.node.frontmatter.templateKey || 'default-template'
+
+    console.log(`Creating page: ${slug} with template: ${templateKey}`)
+
+    createPage({
+      path: slug,
+      component: path.resolve(`src/templates/${templateKey}.js`),
+      context: {
+        id,
+      },
+    })
+  })
+
+  // Create tag pages
+  let tags = []
+  posts.forEach((edge) => {
+    if (_.get(edge, `node.frontmatter.tags`)) {
+      tags = tags.concat(edge.node.frontmatter.tags)
     }
+  })
+  tags = _.uniq(tags)
 
-    const posts = result.data.allMarkdownRemark.edges
-
-    posts.forEach((edge) => {
-      const id = edge.node.id
-      createPage({
-        path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-        ),
-        // additional data can be passed via context
-        context: {
-          id,
-        },
-      })
-    })
-
-    // Tag pages:
-    let tags = []
-    // Iterate through each post, putting all found tags into `tags`
-    posts.forEach((edge) => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    })
-    // Eliminate duplicate tags
-    tags = _.uniq(tags)
-
-    // Make tag pages
-    tags.forEach((tag) => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
-        },
-      })
+  tags.forEach((tag) => {
+    const tagPath = `/tags/${_.kebabCase(tag)}/`
+    createPage({
+      path: tagPath,
+      component: path.resolve(`src/templates/tags.js`),
+      context: {
+        tag,
+      },
     })
   })
 }
@@ -75,11 +73,16 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    // Generate slug based on file path
+    const value = createFilePath({ node, getNode, basePath: `src/pages` })
+
+    // Adjust the slug to account for nested paths
+    const adjustedSlug = value.replace(/^\/src\/pages/, '')
+
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: adjustedSlug,
     })
   }
 }
