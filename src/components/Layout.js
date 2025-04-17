ee617d6ed9f4cac60c09216b42269113969852c0
madj <madj@ailleron.com>
2025-04-17 19:47:12 +0200

    Remove breadcrumb for test #2

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import Footer from "../components/Footer";
-import Breadcrumb from "./Breadcrumb";
+// import Breadcrumb from "./Breadcrumb";
 import Navbar from "../components/navbar2/Navbar";
 import "../style/bulma-style.sass";
 import "../style/custom-style.sass";
